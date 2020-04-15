9c1418cb8b321055f9743a7bb0e0ae47b09fc8f4
Dedi Prakasa <dedprakasa@gmail.com>
2020-04-15 23:51:38 +0700

    - initial creation - remove storyboard

---
+++ InpoMupis/InpoMupis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InpoMupis
+//
+//  Created by Dedi Prakasa on 4/15/20.
+//  Copyright © 2020 Dedi Prakasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
